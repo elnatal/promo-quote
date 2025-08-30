79a89bd193f3102a7c9dd96fdc89d744f49604e9
Elnatal Debebe <elnataldebebe@gmail.com>
2025-08-30 08:08:02 +0300

    Initialize project with Maven wrapper, configuration files, and basic Spring Boot application structure

---
+++ src/main/java/com/elnatal/promo_quoter/PromoQuoterApplication.java
@@ -0,0 +1,13 @@
+package com.elnatal.promo_quoter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoQuoterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromoQuoterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elnatal/promo_quoter/PromoQuoterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elnatal.promo_quoter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoQuoterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
